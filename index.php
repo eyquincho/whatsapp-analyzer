686c9c3f8d83075aa6b6e771a0dd3d68bcbe3033
eyquincho <quinchote@gmail.com>
2017-06-06 11:04:38 +0200

    Update index.php
    Bootstrap por CDN

--- index.php
+++ index.php
@@ -90,6 +90,6 @@
 
 	<!-- script references -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	</body>
 </html>
